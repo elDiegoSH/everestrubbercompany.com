aaa5538279d7628ecd9f94ded66342374e5c421c
DiegoSH <hola@diegosh.com>
2022-02-14 15:01:27 -0600

    Se terminan los JS

---
+++ js/subir.js
@@ -0,0 +1,13 @@
+document.addEvent('domready', function() {
+	new Skyline_ScrollToTop({
+		'image':		'images/back_to_top_btn.png',
+		'text':			'',
+		'title':		'',
+		'className':	'scrollToTop',
+		'duration':		500,
+		'transition':	Fx.Transitions.linear
+	});
+});
+jQuery(window).on('load',  function() {
+				new JCaption('img.caption');
+			});
