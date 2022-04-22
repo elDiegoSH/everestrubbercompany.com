edb4be21a49800023de2c493a58304f618a51efd
Diseño 4 <diegosh84@gmail.com>
2022-04-22 16:44:40 -0500

    terminados los form del footer

---
+++ fullbrac-mining-rubber/formulario2.php
@@ -0,0 +1,72 @@
+﻿<?php
+
+$nombre = "";
+$email = "";
+$mensaje = "";
+$error = "";
+$pagina = "https://everestrubber.com/fullbrac-mining-rubber/";
+
+//VALIDANDO NOMBRE
+if(empty($_POST["nombre2"])){
+    $error = 'Enter your name</br>';
+}else{
+    $nombre = $_POST["nombre2"];
+    $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+    $nombre = trim($nombre);
+    if($nombre==''){
+        $error .= 'Name is empty</br>';
+    }
+}
+//VALIDANDO E-MAIL
+if(empty($_POST["email2"])){
+    $error .= 'Enter your email</br>';
+}else{
+    $email = $_POST["email2"];
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        $error .= 'Enter a true email</br>';
+    }else{
+        $email = filter_var($email,FILTER_SANITIZE_EMAIL);
+    }
+}
+//VALIDANDO MENSAJE
+if(empty($_POST["mensaje2"])){
+    $error .= 'Write your message</br>';
+}else{
+    $mensaje = $_POST["mensaje2"];
+    $mensaje = filter_var($mensaje, FILTER_SANITIZE_STRING);
+    $mensaje = trim($mensaje);
+    if($mensaje==''){
+        $error .= 'Message is empty</br>';
+    }
+}
+
+//CUERPO DEL MENSAJE
+$cuerpo .= "Name: ";
+$cuerpo .= $nombre;
+$cuerpo .= "\n";
+ 
+$cuerpo .= "Email: ";
+$cuerpo .= $email;
+$cuerpo .= "\n";
+ 
+$cuerpo .= "Message: ";
+$cuerpo .= $mensaje;
+$cuerpo .= "\n";
+
+$cuerpo .= "From: ";
+$cuerpo .= $pagina;
+$cuerpo .= "\n";
+
+//DIRECCIÓN
+$enviarA = 'dsanchez@grupoeverest.com.mx, dperez@grupoeverest.com.mx'; 
+$asunto = 'New Quote From Fullbrac Mining Rubber';
+
+//ENVIAR CORREO
+if($error == ''){
+    $success = mail($enviarA,$asunto,$cuerpo,'de: '.$email,);
+    echo 'exito';
+}else{
+    echo $error;
+}
+
+?>
